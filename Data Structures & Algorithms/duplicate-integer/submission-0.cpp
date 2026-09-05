333cc493bccdc7685d7e86eb775630efaaac922f
Varun Arora <varunarorarokzz@gmail.com>
2026-09-05 16:14:38 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (auto num: nums) st.insert(num);
+        return st.size() < nums.size();
+    }
+};
